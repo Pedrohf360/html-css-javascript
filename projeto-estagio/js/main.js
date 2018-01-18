4ca03a753af3be764a1ff1b48ddfaebf38b60080
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-01-18 17:49:27 -0200

    Issue: verificar mediaQueries.

--- projeto-estagio/js/main.js
+++ projeto-estagio/js/main.js
@@ -1,7 +1,10 @@
+    var jsonTxt;
+
      $.getJSON('https://raw.githubusercontent.com/Pedrohf360/html-css-javascript/master/projeto-estagio/js/equipamentos.json', function(data){
     
         jsonTxt = data;
 
+        console.log(jsonTxt);
         /*for (var i = 0; i < 1; i++) {
         alert('ID:' + data[i].ID + '\n' +
               'Name:' + data[i].Name + '\n' +
