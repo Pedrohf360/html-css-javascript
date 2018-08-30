2b670d3b0ac075600f4285bb8a4186a97084db35
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-08-30 10:00:37 -0300

    Testing bulma framework.
    Software tests exercise.

---
+++ testesSoftware/assets/js/main.js
@@ -0,0 +1,24 @@
+function sendForm() {
+    console.log($('#valueA')[0].value);
+    console.log($('#valueB')[0].value);
+    console.log($('#valueC')[0].value);
+    let valA, valB, valC;
+    valA = $('#valueA')[0].value;
+    valB = $('#valueB')[0].value;
+    valC = $('#valueC')[0].value;
+
+   for(let i = 0; i < [])
+
+    $('.modal-content')[0].innerHTML = biggerNumber;
+
+    showModal(biggerNumber);
+}
+
+
+function showModal() {
+    $(".modal").addClass("is-active");
+};
+
+function closeModal() {
+    $(".modal").removeClass("is-active");
+};
